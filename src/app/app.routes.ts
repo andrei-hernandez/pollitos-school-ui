2d05ad72024ec6ed4f50c36b08a8de5d9c6e1ddc
Andrei Hernandez <ahernandez@iwa.com.mx>
2024-12-18 20:44:56 -0600

    chore: add instructions and scaffolding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/instructions',
+    pathMatch: 'full'
+  },
+  {
+    path: 'instructions',
+    loadComponent: () => import('./features/pages/instructions/instructions.component')
+  }
+];
