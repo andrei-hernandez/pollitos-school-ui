3b5ddc935283bd5ab3364dc2df29ea3b1c141719
EduardoGV4 <egonzalezvazquez09@gmail.com>
2024-12-31 11:30:39 -0600

    main page of the both schools

--- src/app/features/Home/pages/home.component.ts
+++ src/app/features/Home/pages/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core'
+import { RouterLink } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterLink, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/features/Institute/pages/gerardoInstitute.component.ts
+++ src/app/features/Institute/pages/gerardoInstitute.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core'
+import { Component, signal } from '@angular/core'
+import { RouterLink, RouterOutlet } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-gerardoInstitute',
-  imports: [],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './gerardoInstitute.component.html',
   styleUrl: './gerardoInstitute.component.css'
 })
 export class GerardoInstituteComponent {
+form = new FormGroup({
+  id: new FormControl(''),
+  name: new FormControl(''),
+  age: new FormControl(''),
+  date: new FormControl('')
+})
+
+students = signal ([
+  {id: 1, name: "Eduardo Emmanuel Gonzalez Vazquez", age: 24, date: "18-Agosto-2018" },
+  { id: 2, name: "Gerardo Emmanuel Gonzalez Orea", age: 44, date: "18-Agosto-1997" },
+  { id: 3, name: "Claudia Vazquez Montero", age: 46, date: "18-Agosto-1995"},
+  { id: 4, name: "Eden Emmanuel Gonzalez Carrasco", age: 20, date: "18-Agosto-2048"},
+]);
+
+handleSubmit(){
+  console.log(this.form.value);
+}
 
 }
